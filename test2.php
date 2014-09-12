3ef6d9d1066b7eb8eaa3650946c8b215add0959a
sho0512 <sho0512@gmail.com>
2014-09-12 14:50:32 +0900

    add test2.php

---
+++ test2.php
@@ -0,0 +1,19 @@
+<?php
+
+// echo
+echo "hello world";
+
+// foreach
+$arr = array('apple', 'orange', 'lemon');
+foreach ($arr as $key=>$val) {
+	echo $val . PHP_EOL;
+}
+
+// if
+if (!empty($val)) {
+	echo "val is " . $val;
+}
+
+// comment
+
+
